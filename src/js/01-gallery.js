dcf77fc60bfed3ea5038b29c950869e27fef75cf
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-01-25 17:02:25 +0800

    try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+document.addEventListener('DOMContentLoaded', function () {
+    const gallery = document.querySelector('.gallery');
+
+    // Render gallery items
+    galleryItems.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('gallery__item');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__link');
+        link.href = item.original;
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.setAttribute('data-source', item.original);
+        image.alt = item.description;
+
+        link.appendChild(image);
+        listItem.appendChild(link);
+        gallery.appendChild(listItem);
+    });
+
+    // Open modal on image click
+    gallery.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (event.target.tagName === 'IMG') {
+            const largeImageUrl = event.target.dataset.source;
+
+            const instance = basicLightbox.create(`
+                <img src="${largeImageUrl}" alt="${event.target.alt}" />
+            `);
+
+            instance.show();
+
+            // Close modal on Escape key press
+            document.addEventListener('keydown', function (e) {
+                if (e.key === 'Escape') {
+                    instance.close();
+                }
+            });
+        }
+    });
+});
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryList = document.querySelector('.gallery');
+
+    const createGallery = items => {
+        return items.map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+        }).join('');
+    };
+
+    const photosMarkup = createGallery(galleryItems);
+    galleryList.innerHTML = photosMarkup;
+
+    // Initialize SimpleLightbox
+    const gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+});
